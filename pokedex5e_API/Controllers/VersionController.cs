44306cddf8a5f7bb54a83fba1a7e5f56e3e04f05
DAUCHOT Joachim <dauchot.joachim@gmail.com>
2024-02-01 14:00:41 +0100

    Version controller

---
+++ pokedex5e_API/Controllers/VersionController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace pokedex5e_API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class VersionController : ControllerBase
+    {
+        [HttpGet] public async Task<string> Get()
+        {
+            return "1.0.0";
+        }
+    }
+}
