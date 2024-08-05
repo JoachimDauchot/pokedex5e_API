8870f4868a9efbb99bbede54cb898fbf5713d0d6
DAUCHOT Joachim <dauchot.joachim@gmail.com>
2024-08-05 18:03:39 +0200

    fixed some json data

--- pokedex5e_API/Resources/Data/Version.cs
+++ pokedex5e_API/Resources/Data/Version.cs
@@ -4,5 +4,5 @@ public class Version
 {
     //update this manually upon change!!
     //there will be no automatic build en no automatic versioning
-    public static readonly string Number = "\"1.0.8\"";
+    public static readonly string Number = "\"1.0.14\"";
 }
